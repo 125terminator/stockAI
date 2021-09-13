b1cf8869d32387b7fc189dd4a59d233ff5830ca7
Priyank Kumar <32513375+125terminator@users.noreply.github.com>
2021-09-13 23:42:51 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
+import numpy as np
+import pandas as pd
 
-GEN_NUM = 0
+from neural_network import *
+from robo import *
+from ga import *
+
+df = pd.read_csv('data/reliance.csv')
+df = df[:10000]
 
 def main(ann):
-    ##########
-    global GEN_NUM
     robos = {}
 
     ge = {}
     nets = {}
     for robo_id in range(0, len(ann)):
         nets[robo_id] = net
-        robos[robo_id] = Robo(ann[robo_id], 100000)
+        robos[robo_id] = Robo(ann=ann[robo_id], df=df, money=100000)
         ge[robo_id] = robos[robo_id].fitness()
-    # fp_fitness.write('{}\n'.format(best_fitness))
-    # fp_food.write('{}\n'.format(best_robo.num_food))
-    # if GEN_NUM%10 == 0:
-    #     if not os.path.exists('save'):
-    #       os.makedirs('save')
-    #     saveDict = {'robo' : best_robo.robo_locations, 'food' : best_robo.food_locations}
-    #     # save.generationBest[GEN_NUM] = saveDict
-    #     with open('save/save{}.json'.format(GEN_NUM), 'w') as fp:
-    #         json.dump(saveDict, fp)
-    GEN_NUM += 1
     return ge 
 
 # np.random.seed(1999)

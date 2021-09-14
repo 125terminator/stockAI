51c1b13bd60c195a2d4a2435d5c57d10ca5238df
Priyank Kumar <32513375+125terminator@users.noreply.github.com>
2021-09-15 01:04:36 +0530

    adding time parameter input in ann

--- robo.py
+++ robo.py
@@ -1,5 +1,7 @@
+from io import DEFAULT_BUFFER_SIZE
 from scipy.stats import zscore
 from broker_calc import get_net_profit
+from datetime import datetime
 
 
 class Robo:
@@ -11,6 +13,7 @@ class Robo:
         # self.bought -> [has stocks, number of stocks, price of each stock]
         self.bought = [False, 0, 0]
         self.moving_averages = []
+        self.time_ind = []
     
     def fitness(self):
         self.prepare_inputs()
@@ -18,7 +21,11 @@ class Robo:
         # 0 -> buy if money
         # 1 -> sell if has stocks
         # 2 -> hold
-        for i in range(60, output.shape[1]):
+        start_date_index = 60
+        while df.Date[start_date_index][12] != 9 or df.Date[start_date_index][14:16]!=15:
+            start_date_index += 1
+
+        for i in range(start_date_index, output.shape[1]):
             ind = output[:, i].argmax()
 
             if ind == 0:
@@ -38,16 +45,27 @@ class Robo:
                     self.money += holdings
                     self.bought = [False, 0, 0]
 
-            elif ind == 2:
-                pass
+            
+            if df.Date[i][11:13]==15 and df.Date[i][14:16]==29:
+                self.bought = [False, 0, 0]
 
         return self.money
 
     def prepare_inputs(self):
         self.get_moving_averages()
 
+    def get_time_as_absindex(self):
+        # return 09:15 as 1, 09:16 as 2, so on
+        init_time = datetime.fromisoformat('2015-03-02T09:14:00')
+        time = []
+        for i in range(len(df)):
+            ind = datetime.fromisoformat(df.Date[i]) - init_time
+            time.append(ind.seconds // 60)
+        
+        self.time_ind = np.array(time)
+
     def get_moving_averages(self):
-        periods = [1, 3, 5, 7, 11, 15, 19, 23, 27, 35, 41, 50, 61]
+        periods = [1, 3, 5, 7, 11, 15, 19, 23, 27, 35, 41, 50, 61, 128]
         moving_averages = []
         for period in periods:
             m = self.df.Close.rolling(period).mean()

208714a51f098dca3454d230a692e340af562265
Priyank Kumar <32513375+125terminator@users.noreply.github.com>
2021-09-15 01:36:45 +0530

    added time as input in ann

--- robo.py
+++ robo.py
@@ -2,6 +2,8 @@ from io import DEFAULT_BUFFER_SIZE
 from scipy.stats import zscore
 from broker_calc import get_net_profit
 from datetime import datetime
+from dateutil.parser import parse
+import numpy as np
 
 
 class Robo:
@@ -14,15 +16,16 @@ class Robo:
         self.bought = [False, 0, 0]
         self.moving_averages = []
         self.time_ind = []
+        self.inputs = []
     
     def fitness(self):
         self.prepare_inputs()
-        output = self.ann.forward_propagation(self.moving_averages)
+        output = self.ann.forward_propagation(self.inputs)
         # 0 -> buy if money
         # 1 -> sell if has stocks
         # 2 -> hold
         start_date_index = 60
-        while df.Date[start_date_index][12] != 9 or df.Date[start_date_index][14:16]!=15:
+        while df.Date[start_date_index][12] != '9' or df.Date[start_date_index][14:16]!='15':
             start_date_index += 1
 
         for i in range(start_date_index, output.shape[1]):
@@ -46,23 +49,25 @@ class Robo:
                     self.bought = [False, 0, 0]
 
             
-            if df.Date[i][11:13]==15 and df.Date[i][14:16]==29:
+            if df.Date[i][11:13]=='15' and df.Date[i][14:16]=='29':
                 self.bought = [False, 0, 0]
 
         return self.money
 
     def prepare_inputs(self):
         self.get_moving_averages()
-
+        self.get_time_as_absindex()
+        self.inputs = np.concatenate((self.time_ind, self.moving_averages))
+    
     def get_time_as_absindex(self):
         # return 09:15 as 1, 09:16 as 2, so on
-        init_time = datetime.fromisoformat('2015-03-02T09:14:00')
+        init_time = parse('2015-03-02T09:14:00+0530')
         time = []
         for i in range(len(df)):
-            ind = datetime.fromisoformat(df.Date[i]) - init_time
+            ind = parse(df.Date[i]) - init_time
             time.append(ind.seconds // 60)
-        
-        self.time_ind = np.array(time)
+        self.time_ind = zscore(time)
+        self.time_ind = np.reshape(self.time_ind, (1, self.time_ind.shape[0]))
 
     def get_moving_averages(self):
         periods = [1, 3, 5, 7, 11, 15, 19, 23, 27, 35, 41, 50, 61, 128]

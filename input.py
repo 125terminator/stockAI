4024b28e21e92a21c944c041cdd861624a3a3f98
Priyank Kumar <32513375+125terminator@users.noreply.github.com>
2021-09-21 00:51:29 +0530

    added rsi as ann input

--- input.py
+++ input.py
@@ -1,17 +1,40 @@
-from scipy.stats import zscore
-
+import numpy as np
 class Inputs:
     def __init__(self, df):
         self.df = df
         self.moving_averages = []
+        self.rsi_list = []
         self.inputs = []
         self.prepare_inputs()
 
     def prepare_inputs(self):
-        self.get_moving_averages()
-        self.inputs = self.moving_averages
+        self.compute_moving_averages()
+        self.compute_rsi()
+        self.inputs = np.concatenate((self.moving_averages, self.rsi_list), axis=0)
+
+    def ema(self, series, n):
+        return series.ewm(span=n, min_periods=n).mean()
+
+    def rsi(self, n=14):
+        diff = self.df.Close.diff(1)
+        which_dn = diff < 0
+        up, dn = diff, diff*0
+        up[which_dn], dn[which_dn] = 0, -up[which_dn]
+        emaup = self.ema(up, n)
+        emadn = self.ema(dn, n)
+        rsi = 100 * emaup / (emaup + emadn)
+        return np.array(rsi)
+
+    def compute_rsi(self):
+        periods = [15, 27, 41, 61, 128, 375]
+        self.rsi_list = []
+        for period in periods:
+            m = self.rsi(period)
+            self.rsi_list.append(m)
+        
+        self.rsi_list = np.array(self.rsi_list)
     
-    def get_moving_averages(self):
+    def compute_moving_averages(self):
         periods = [1, 3, 5, 7, 11, 15, 19, 23, 27, 35, 41, 50, 61, 128, 375]
         moving_averages = []
         for period in periods:
--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from input import Inputs
 
 df = pd.read_csv('data/reliance.csv')
 # 375 ind is start of 9:15 and 10088 time is 15:29
-df = df[:10088]
+n = 375*41
+df = df[:n]
 df.index = np.arange(0, df.shape[0])
 df["Time"] = df.Date
 df.Date = df.Date.apply(lambda x: (int(x[11:13])-9)*60 + int(x[14:16]) - 14)

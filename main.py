429c5cf588f126e0278e1da496c0a49a65cbdb87
Priyank Kumar <32513375+125terminator@users.noreply.github.com>
2021-10-05 22:14:37 +0530

    bug fixes

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ def main(ann):
     ge = {}
     cur_time = time.time()
     for robo_id in range(0, len(ann)):
-        if ann.fitness == False:
+        if ann[robo_id].fitness == False:
             robos[robo_id] = Robo(ann=ann[robo_id], df=df, money=100000, inputs=ann_inputs.inputs, logger=logger)
             ge[robo_id] = robos[robo_id].fitness()
-            ann.fitness = ge[robo_id]
+            ann[robo_id].fitness = ge[robo_id]
         else:
-            ge[robo_id] = ann.fitness
+            ge[robo_id] = ann[robo_id].fitness
     logger.close()
     print(time.time() - cur_time)
     return ge 
@@ -44,4 +44,4 @@ def main(ann):
 
 x = np.random.rand(18, 1)
 y = np.random.rand(3, 1)
-bestPop = GA(x, y, n_h=[20, 12], generations=100000, popSize=100, eliteSize=10, main=main, mutationRate=0.5)
+bestPop = GA(x, y, n_h=[20, 12], generations=100000, popSize=200, eliteSize=20, main=main, mutationRate=0.5)
